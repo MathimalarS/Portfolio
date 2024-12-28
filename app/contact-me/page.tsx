cdec0aa1d3e48b089a609edb37413bd02ec1fa34
Mathimalar <malars0409@gmail.com>
2024-12-28 21:40:23 +0530

    Complete project

--- app/contact-me/page.tsx
+++ app/contact-me/page.tsx
@@ -8,7 +8,7 @@ const Page = () => {
       className="w-screen h-screen bg-cover bg-center flex items-center justify-center"
     >
       <div
-        style={{ backgroundImage: "url(bg3.png)" }}
+        style={{ backgroundImage: "url(bg3.jpg)" }}
         className="h-[60%] w-[80%] relative bg-cover bg-center rounded-xl border border-white"
       >
         <div className="absolute left-20 bottom-16 w-[70%] md:w-[30%]">
